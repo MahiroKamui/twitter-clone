1afc3691e9e12f5c20fc3b6d994594d8c667540c
MahiroKamui <alexibenjamin98@gmail.com>
2026-04-02 07:03:49 +0300

    Added POST function and adding accounts to the db functionality

---
+++ app/api/users/route.ts
@@ -0,0 +1,20 @@
+import Database from 'better-sqlite3'
+const db = new Database('./db/database.db')
+
+export async function POST(req: Request) {
+    const data = await req.json()
+    const userData = {
+        name: data.name,
+        username: data.username,
+        password: data.password
+    }
+    try {
+        const insert = db.prepare('INSERT INTO users (name, username, password) VALUES (?, ?, ?)')
+        insert.run(userData.name, userData.username, userData.password)
+    } catch (err) {
+        console.log(err)
+    }
+
+    return Response.json('Your account has been created!')
+}
+
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { FormEvent, useState } from "react"
 
 
@@ -8,29 +7,34 @@ export default function page() {
     const [name, setName] = useState<string>("")
     const [username, setUsername] = useState<string>("")
     const [password, setPassword] = useState<string>("")
-    
-    
+
+
     function signUp(e: any) {
         e.preventDefault()
-        fetch('api/users/route.ts', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify("")
-        })
+        try {
+            fetch('api/users', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name: name, username: username, password: password })
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
-    
+
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-red-100 bg-size-[20%] font-sans bg-[url(/images/authentication/register/flower.png)]">
             <div className="flex flex-col items-center justify-center bg-pink-200 h-70 w-170 shadow-xl border border-red-400 gap-8">
                 <span className="text-3xl max-sm:text-4xl">Create your account</span>
-                <form  onSubmit={signUp} className="flex flex-col max-sm:w-120 gap-0.5">
-                    <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700"/>
-                    <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700"/>
-                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700"/>
-                    <input type="submit"/>
+                <form onSubmit={signUp} className="flex flex-col max-sm:w-120 gap-0.5">
+                    <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700" />
+                    <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700" />
+                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} className="bg-pink-300 w-65 h-10 pl-2 border-2 border-pink-700" />
+                    <input type="submit" />
                 </form>
-            </div> 
+            </div>
         </div>
     )
 }
